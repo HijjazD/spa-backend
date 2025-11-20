0fc54ac5aa195d750cb4a8fc42dd634e9c2307dd
HijjazD <154423459+HijjazD@users.noreply.github.com>
2025-11-21 00:58:41 +0800

    Update CORS allowed origins for production

--- src/main/java/com/spa/spa/configs/WebSecurityConfig.java
+++ src/main/java/com/spa/spa/configs/WebSecurityConfig.java
@@ -64,7 +64,7 @@ public class WebSecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource(){
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173"));
+        configuration.setAllowedOrigins(Arrays.asList("https://nailaaziz.vercel.app"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(Arrays.asList("*"));
         configuration.setAllowCredentials(true);
